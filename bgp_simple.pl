60f3a853678658d5ba8a95e95f63607f4243e072
the.einval <the.einval@6d8ee946-d3fd-11dd-b6b4-918b84bbd919>
2008-12-31 14:03:39 +0000

    Some code cleanups, added support to display BGP ERROR codes

--- bgp_simple.pl
+++ bgp_simple.pl
@@ -18,17 +18,66 @@ bgp_simple.pl:
 	     	-peerip IP address		# (mandatory) peer IP address
 		-peeras	ASNUMBER		# (mandatory) peer AS number
  		[-v]                       	# (optional) provide verbose output to STDOUT, use twice to get debugs
- 		[-f file]                       # (optional) prefixes to advertise (bgpdump formatted)
+ 		[-p file]                       # (optional) prefixes to advertise (bgpdump formatted)
+                [-f regex]                 	# (optional) filter on input prefixes (requires -p)
 		[-o file]			# (optional) write all sent and received UPDATE messages to file
                 [-m number]                     # (optional) maximum number of prefixes to advertise
                 [-n IP address]                 # (optional) next hop self, overrides original value
-                [-dry]                 		# (optional) dry run; dont build adjacency, but check prefix file (requires -f)
+                [-dry]                 		# (optional) dry run; dont build adjacency, but check prefix file (requires -p)
 
 Without any prefix file to import, only an adjacency is established and the advertised NLRIs,
 including their attributes, are logged.
 
 EOF
 
+my %BGP_ERROR_CODES = (
+			1 => { 		__NAME__ => "Message Header Error", 
+					0 => "n/a",
+				 	1 => "Connection Not Synchronized", 
+					2 => "Bad Message Length", 
+					3 => "Bad Message Type"
+			},
+			2 => {		__NAME__ => "OPEN Message Error",
+					0 => "n/a",
+					1 => "Unsupported Version Number",
+					2 => "Bad Peer AS",
+					3 => "Bad BGP Identifier",
+					4 => "Unsupported Optional Parameter",
+					5 => "[Deprecated], see RFC4271",
+					6 => "Unacceptable Hold Time"
+			},
+			3 => { 		__NAME__ => "UPDATE Message Error",
+					0 => "n/a",
+					1 => "Malformed Attribute List",
+					2 => "Unrecognized Well-known Attribute",
+					3 => "Missing Well-known Attribute",
+					4 => "Attribute Flags Error",
+					5 => "Attribute Length Error",
+					6 => "Invalid ORIGIN Attribute",
+					7 => "[Deprecated], see RFC4271",
+					8 => "Invalid NEXT_HOP Attribute",
+					9 => "Optional Attribute Error",
+					10=> "Invalid Network Field",
+					11=> "Malformed AS_PATH"
+			},
+			4 => {		__NAME__ => "Hold Timer Expired",
+					0 => "n/a"
+			},
+			5 => {		__NAME__ => "Finite State Machine Error".
+					0 => "n/a"
+			},
+			6 => {		__NAME__ => "Cease",
+					0 => "n/a",
+					1 => "Maximum Number of Prefixes Reached",
+					2 => "Administrative Shutdown",
+					3 => "Peer De-configured",
+					4 => "Administrative Reset",
+					5 => "Connection Rejected",
+					6 => "Other Configuration Change",
+					7 => "Connection Collision Resolution",
+					9 => "Out of Resources"
+			}
+);
 my $infile;
 my $outfile;
 my $prefix_limit;
@@ -41,13 +90,15 @@ my $myas;
 my $myip;
 my $peeras;
 my $peerip;
+my %regex_filter;
 
 GetOptions( 	'help' 		=> sub{ my_debug("m","$help"); exit; },   
 		'm=s' 		=> \$prefix_limit,
 		'v+' 		=> \$verbose,
 		'dry' 		=> \$dry,
 		'n:s' 		=> \$next_hop_self,
-		'f=s' 		=> \$infile,
+		'p=s' 		=> \$infile,
+		'f=s' 		=> \%regex_filter,
 		'o=s' 		=> \$outfile,
 		'myas=s' 	=> \$myas,
 		'myip=s' 	=> \$myip,
@@ -94,8 +145,32 @@ close (INPUT);
 die "Cannot open file $outfile" if ( ($outfile) && !( open (OUTPUT,">$outfile") ) ); 
 close (OUTPUT);
 
+# KEY=REGEX
+# KEY is one of
+# 	NEIG		originating neighbor
+# 	NLRI		NLRI/prefix(es)
+# 	ASPT		AS_PATH
+#	ORIG		ORIGIN
+#	NXHP		NEXT_HOP
+#	LOCP		LOCAL_PREF
+#	MED		MULTI_EXIT_DISC
+#	COMM		COMMUNITY
+#	ATOM		ATOMIC_AGGREGATE
+#	AGG		AGGREGATOR
+# REGEX is a perl regular expression to be expected in a match statement (m/REGEX/)
+
+if (%regex_filter)
+{
+	foreach my $key (keys %regex_filter)
+	{
+		print "$key = $regex_filter{$key} \n";
+	}
+#	my @regex_segments = split /;/,$regex_string;
+#	my $p_regex = eval { qr/$string_regex/ } || die "Regex $string_regex is bogus.\n";
+}
+
 my_debug ("m", "---------------------------------------- CONFIG SUMMARY --------------------------------------------------\n");
-my_debug ("m", "Configured for an $peer_type session between me (AS$myas, $myip) and peer (AS$peeras, $peerip).\n");
+my_debug ("m", "Configured for an $peer_type session between me (ASN$myas, $myip) and peer (ASN$peeras, $peerip).\n");
 my_debug ("m", "Generating verbose output, level $verbose.\n") if $verbose;
 my_debug ("m", "Will use prefixes from file $infile.\n") if $infile;
 my_debug ("m", "Will write sent and received UPDATEs to file $outfile.\n") if $outfile;
@@ -218,11 +293,11 @@ sub my_timer_callback
 	{ 	
 		my_debug ("m","Sending full update.\n");
 
+		$full_update = 1;
 		my_update_from_file($peer);
 
 		my_debug ("m", "Full update sent.\n");
 
-		$full_update = 1;
 	}
 }
 
@@ -294,7 +369,7 @@ sub my_update_callback
 sub my_notification_callback
 {
 	my ($peer) = shift(@_);
-	my ($msg)  = shift(@_);
+	my ($msg) = shift(@_);
 
        	my $peerid =  $peer->peer_id();
         my $peeras =  $peer->peer_as();
@@ -302,7 +377,12 @@ sub my_notification_callback
 	my $error_subcode = $msg->error_subcode();
 	my $error_data = $msg->error_data();
 
-	my_debug ("d", "Notification received, $error_code, $error_subcode, $error_data.\n");
+	my $error_msg = %BGP_ERROR_CODES->{ $error_code }->{ __NAME__ };
+	my_debug ("e", "NOTIFICATION received: TYPE [$error_msg]");
+	my_debug ("e", " SUBCODE [" . %BGP_ERROR_CODES->{ $error_code }->{ $error_subcode } . "]") 	if ($error_subcode);
+	my_debug ("e", " ADDITIONAL_PEER_DATA [" .  unpack ("H*", $error_data) . "]") 			if ($error_data);
+	my_debug ("e", "\n");
+
 }
 
 sub my_error_callback
@@ -316,7 +396,11 @@ sub my_error_callback
 	my $error_subcode = $msg->error_subcode();
 	my $error_data = $msg->error_data();
 
-	my_debug ("e", "Error received, $error_code, $error_subcode, $error_data.\n");
+	my $error_msg = %BGP_ERROR_CODES->{ $error_code }->{ __NAME__ };
+	my_debug ("e", "ERROR occured: TYPE [$error_msg]");
+	my_debug ("e", " SUBCODE [" . %BGP_ERROR_CODES->{ $error_code }->{ $error_subcode } . "]") if ($error_subcode);
+	my_debug ("e", " DATA [" .  unpack ("H*", $error_data) . "]") if ($error_data);
+	my_debug ("e", "\n");
 }
 
 sub my_update_from_file
@@ -328,8 +412,16 @@ sub my_update_from_file
 
 	while (<INPUT>)
 	{
-		my($line) = $_;
+		my $line = $_;
 		chomp($line);
+			
+		my $p_regex;
+		if (($p_regex) && ($line !~ /$p_regex/))
+		{
+			my_debug ("d", "Prefix failed regex match, skipping.\n");
+			next; 
+		}; 
+		
 		my @nlri = split /\|/,$line; 
 
 		if (my_checkprefix($nlri[5])) { my_debug ("e", "Prefix [ $nlri[5] ] failed because of wrong prefix format.\n"); next; }; 
